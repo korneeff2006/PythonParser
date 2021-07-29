9f702ab445e01fe814df84f169310ad2a453c30a
Konstantin Korneev <korneeff2006@yandex.ru>
2021-07-29 11:34:39 +0300

    full parsing complete

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ url = 'https://quotes.toscrape.com/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 quotes = soup.find_all('span', class_='text')
+authors = soup.find_all('small', class_='author')
+tags = soup.find_all('div', class_='tags')
 
-# For commits
-# New commit for push to the GitHub
-for quote in quotes:
-    print(quote.text)
+for i in range(0, len(quotes)):
+    print(quotes[i].text)
+    print('--' + authors[i].text)
+    tagsforquote = tags[i].find_all('a', class_='tag')
+    for tagforquote in tagsforquote:
+        print(tagforquote.text)
+    print('\n')
